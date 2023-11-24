fdba45a1cb3857a968581b3f0c40633952411092
aalexebr <alex.ebr@icloud.com>
2023-11-24 15:15:04 +0100

    milestone 3

--- src/main/java/org/java/db/Main.java
+++ src/main/java/org/java/db/Main.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.Scanner;
 
 public class Main {
 	
@@ -13,10 +14,12 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		milestone1();
+//		milestone2();
+		
+		milestone3();
 	}
 	
-	private static void milestone1() {
+	private static void milestone2() {
 		System.out.println("m1");
 		
 		try (Connection con = DriverManager.getConnection(url, user, pws)){
@@ -57,6 +60,66 @@ public class Main {
 				    }
 			 }
 			
+			
+		}
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		
+	}
+	
+	private static void milestone3() {
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("what country do you want to search?");
+		String userQuery = in.nextLine();
+		
+		in.close();
+		
+		try (Connection con = DriverManager.getConnection(url, user, pws)){
+			
+			String sqlQuery = " SELECT countries.name as country_name, countries.country_id as country_id, regions.name as regon_name, continents.name as continent_name "
+					+ " FROM continents "
+					+ " JOIN regions "
+					+ " ON continents.continent_id = regions.continent_id "
+					+ " JOIN countries "
+					+ " ON regions.region_id = countries.region_id "
+					+ " JOIN country_languages "
+					+ " ON country_languages.country_id = countries.country_id "
+					+ " JOIN languages "
+					+ " ON languages.language_id = country_languages.language_id "
+					+ " WHERE countries.name LIKE ? "
+					+ " GROUP BY countries.country_id; ";
+			
+				try (PreparedStatement ps = con.prepareStatement(sqlQuery)) {
+				
+						ps.setString(1, "%"+userQuery+"%");
+						
+							    try(ResultSet rs = ps.executeQuery()){
+							    	
+							    	int counter = 0;
+							    	
+							    	while(rs.next()) {
+							    		
+							    		String country = rs.getString(1);
+							    		int countryId = rs.getInt(2);
+							    		String region = rs.getString(3);
+							    		String continent = rs.getString(4);
+							    		counter ++;
+							    		
+							    		System.out.println("country: "+country+" - "
+							    				+"country id: "+ countryId+ " - "
+							    				+"region: "+region+" - "
+							    				+"continent: "+continent);
+							    		
+							    		
+							    	}
+							    	System.out.println("counter: "+ counter);
+							    }
+						 }
+							
 			
 		}
 		catch (Exception e) {
